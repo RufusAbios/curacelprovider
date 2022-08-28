17eddaf27a7893206ac9a661b9611f0f0084226a
RufusAbios <abioritsegbemi.rufus@gmail.com>
2022-08-28 18:00:49 +0100

    added login test and assertion

---
+++ cypress/e2e/logintest.cy.js
@@ -0,0 +1,19 @@
+describe('curacelprovider', () => {
+  it('logintest', () => {
+    cy.visit('https://ironman.claims.curacel.co/#/login')
+
+    cy.get('#input-live').type('user1@hospital1.com')
+
+    cy.get(':nth-child(2) > .form-control').type('Password123_')
+
+    cy.get('button').click()
+
+    cy.wait(5000)
+
+    cy.get('.navbar-brand').contains('Dashboard')
+
+
+
+
+  })
+})
